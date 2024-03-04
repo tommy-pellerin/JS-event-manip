1df199b364807ae8de5c10914e119fb45ef91359
tommy-pellerin <tommytp@hotmail.fr>
2024-03-04 12:18:22 +0100

    exo5 ok

--- script.js
+++ script.js
@@ -31,12 +31,38 @@ burgerMenu.addEventListener("click",dropBurgerMenu);
 
 //F3
 let firstCardEditBtn = document.querySelectorAll("button.btn-outline-secondary")[0]
-console.log(firstCardEditBtn);
-let changeColor = function(){
+// console.log(firstCardEditBtn);
+let textColorRed = function(){
   // firstCardEditBtn.style.Color = "red"; //ne fonctionne pas
   firstCardEditBtn.setAttribute("style","color:red")
 
 }
-firstCardEditBtn.addEventListener("click",changeColor);
+firstCardEditBtn.addEventListener("click",textColorRed);
 
 //F4
+let secondCardEditBtn = document.querySelectorAll("button.btn-outline-secondary")[1]
+// console.log(secondCardEditBtn);
+let TextColorGreen = function(){
+  // secondCardEditBtn.setAttribute("style","") //ceci ne sert à rien, il rentre en conflit avec .style.color
+  if (secondCardEditBtn.style.color === "green"){
+    secondCardEditBtn.style.color = "";
+  } else {
+    secondCardEditBtn.style.color = "green";
+  }
+
+}
+secondCardEditBtn.addEventListener("click",TextColorGreen);
+
+//F5
+let cdnLink = document.getElementsByTagName("link")[0];
+// console.log(cdnLink);
+let header = document.getElementsByTagName("header")[0];
+// console.log(header);
+let removeLink = function(){
+  if (cdnLink.disabled === true) {
+    cdnLink.disabled = false;
+  } else {
+    cdnLink.disabled = true;
+  }
+}
+header.addEventListener("dblclick",removeLink);
