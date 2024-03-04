80b8056ce042ff4ddc91204334c24239a3337fc5
tommy-pellerin <tommytp@hotmail.fr>
2024-03-04 11:18:57 +0100

    exo 3 ok

--- script.js
+++ script.js
@@ -8,13 +8,35 @@ let showClick = function(){
 footer.addEventListener("click",showClick);
 */
 
-//F2
+//F1-bis
 let counter = 0
 let footer = document.getElementsByTagName("footer")[0];
-console.log(footer);
+// console.log(footer);
 let showClick = function(){
   counter += 1
   console.log("Je clique! numéro " + counter)
   
 };
 footer.addEventListener("click",showClick);
+
+//F2
+let navbarHeader = document.getElementById("navbarHeader");
+// console.log(navbarHeader);
+let burgerMenu = document.querySelectorAll("span.navbar-toggler-icon")[0];
+// console.log(burgerMenu);
+let dropBurgerMenu = function(){
+  navbarHeader.classList.toggle("collapse");
+}
+burgerMenu.addEventListener("click",dropBurgerMenu);
+
+//F3
+let firstCardEditBtn = document.querySelectorAll("button.btn-outline-secondary")[0]
+console.log(firstCardEditBtn);
+let changeColor = function(){
+  // firstCardEditBtn.style.Color = "red"; //ne fonctionne pas
+  firstCardEditBtn.setAttribute("style","color:red")
+
+}
+firstCardEditBtn.addEventListener("click",changeColor);
+
+//F4
